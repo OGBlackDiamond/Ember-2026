e251239e6921015e60f04a950dedea6a0df51d80
Caden Feller <caden.feller@gmail.com>
2026-02-20 18:02:48 -0800

    added limelight stuff

--- src/main/java/frc/robot/util/Auto.java
+++ src/main/java/frc/robot/util/Auto.java
@@ -114,7 +114,7 @@ public class Auto {
                 midFromMid.cmd()
             )
         );
-    
+
         return fromMid;
     }
 }
---
+++ src/main/java/frc/robot/util/Vision.java
@@ -0,0 +1,20 @@
+package frc.robot.util;
+
+
+public class Vision {
+
+
+    public enum PIPELINE_TYPE {
+        APRILTAGS,
+        COLOR
+    }
+
+    public Vision() {
+
+    }
+
+    public void setPipeline(PIPELINE_TYPE type) {
+    //TODO: do stuff
+    }
+
+}
